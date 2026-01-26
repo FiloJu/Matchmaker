1a03b424a9a8a894525699f78bd040b2e1fd1ebc
Filomena Jurgaityte <filomena.jurgaityte@gmail.com>
2026-01-26 21:35:50 +0100

    add static and refactor

--- index.php
+++ index.php
@@ -7,37 +7,46 @@ const RESULT_POSSIBILITIES = [RESULT_WINNER, RESULT_LOSER, RESULT_DRAW];
 
 class Encounter
 {
-    public function probabilityAgainst(int $levelPlayerOne, int $againstLevelPlayerTwo)
+    public static function probabilityAgainst(Player $playerOne, Player $playerTwo): float
     {
-        return 1 / (1 + (10 ** (($againstLevelPlayerTwo - $levelPlayerOne) / 400)));
+        return 1 / (1 + (10 ** (($playerTwo->level - $playerOne->level) / 400)));
     }
 
-    public function setNewLevel(int &$levelPlayerOne, int $againstLevelPlayerTwo, int $playerOneResult)
+    public static function setNewLevel(Player $playerOne, Player $playerTwo, int $playerOneResult): void
     {
         if (!in_array($playerOneResult, RESULT_POSSIBILITIES)) {
             trigger_error(sprintf('Invalid result. Expected %s', implode(' or ', RESULT_POSSIBILITIES)));
         }
 
-        $levelPlayerOne += (int) (32 * ($playerOneResult - $this->probabilityAgainst($levelPlayerOne, $againstLevelPlayerTwo)));
+        $playerOne->level += round(32 * ($playerOneResult - self::probabilityAgainst($playerOne, $playerTwo)));
     }
 }
 
-$greg = 400;
-$jade = 800;
-$encounter = new Encounter();
+class Player
+{
+    public int $level;
+}
+
+$greg = new Player;
+$jade = new Player;
+
+$greg->level = 505;
+$jade->level = 800;
+
+
 echo sprintf(
     'Greg à %.2f%% chance de gagner face a Jade',
-    $encounter->probabilityAgainst($greg, $jade) * 100
+    Encounter::probabilityAgainst($greg, $jade) * 100
 ) . PHP_EOL;
 
 // Imaginons que greg l'emporte tout de même.
-$encounter->setNewLevel($greg, $jade, RESULT_WINNER);
-$encounter->setNewLevel($jade, $greg, RESULT_LOSER);
+Encounter::setNewLevel($greg, $jade, RESULT_WINNER);
+Encounter::setNewLevel($jade, $greg, RESULT_LOSER);
 
 echo sprintf(
     'les niveaux des joueurs ont évolués vers %s pour Greg et %s pour Jade',
-    $greg,
-    $jade
+    $greg->level,
+    $jade->level
 );
 
 exit(0);
