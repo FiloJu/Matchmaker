73720109fd5fbef271e8f628068e2de58d290484
Filomena Jurgaityte <filomena.jurgaityte@gmail.com>
2026-02-18 08:29:06 +0100

    add AbstractPlayer class

--- index.php
+++ index.php
@@ -32,23 +32,34 @@ class Lobby
     }
 }
 
-class Player
+abstract class AbstractPlayer
 {
-    public function __construct(protected string $name, protected float $ratio = 400.0)
+    public function __construct(public string $name = 'anonymous', public float $ratio = 400.0)
     {
     }
 
+    abstract public function getName(): string;
+
+    abstract public function getRatio(): float;
+
+    abstract protected function probabilityAgainst(self $player): float;
+
+    abstract public function updateRatioAgainst(self $player, int $result): void;
+}
+
+class Player extends AbstractPlayer
+{
     public function getName(): string
     {
         return $this->name;
     }
 
-    protected function probabilityAgainst(self $player): float
+    protected function probabilityAgainst(AbstractPlayer $player): float
     {
         return 1 / (1 + (10 ** (($player->getRatio() - $this->getRatio()) / 400)));
     }
 
-    public function updateRatioAgainst(self $player, int $result): void
+    public function updateRatioAgainst(AbstractPlayer $player, int $result): void
     {
         $this->ratio += 32 * ($result - $this->probabilityAgainst($player));
     }
@@ -61,7 +72,7 @@ class Player
 
 class QueuingPlayer extends Player
 {
-    public function __construct(Player $player, protected int $range = 1)
+    public function __construct(AbstractPlayer $player, protected int $range = 1)
     {
         parent::__construct($player->getName(), $player->getRatio());
     }
