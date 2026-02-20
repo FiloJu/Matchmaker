4174be165a9465779a437213d0941e2f20ec95db
Filomena Jurgaityte <filomena.jurgaityte@gmail.com>
2026-02-20 10:23:12 +0100

    add Lobby interface

---
+++ src/MatchMaker/LobbyInterface.php
@@ -0,0 +1,14 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Src\MatchMaker;
+
+use Src\MatchMaker\Player\PlayerInterface;
+
+interface LobbyInterface
+{
+    public function isPlaying(PlayerInterface $player): bool;
+
+    public function addPlayer(PlayerInterface $player): void;
+}
